2a907effd52b64bd9f09297f4f54bb2d23ec5a6e
arm2arm <arm2arm@ac86fbc6-5caf-11de-992f-fdd7e25b9163>
2009-06-22 12:19:35 +0000

    Final version with automated extraction of many blocks. ToDO: we can make memory optimization later.

--- trunk/gad2extract/lib/Extractor.cpp
+++ trunk/gad2extract/lib/Extractor.cpp
@@ -26,7 +26,9 @@ public:
     safeFree(pID);
     safeFree(pPOS);
     safeFree(pTYPE);
-    delete m_gin;
+    safeFree(pVEL);
+    safeFree(pMASS);
+    safeFreeOne( m_gin);
   };
   void GetCom(std::vector<int> &trID, double *COM)
   {
@@ -146,9 +148,19 @@ public:
     
     nelem=0;
     safeFree(pPOS);
+    safeFree(pVEL);
+    safeFree(pID);
+    safeFree(pMASS);
     string blname = "POS ";
     nelem=m_gin->read_whole_block((char*&)pPOS, 
 			"POS ")/3;
+    nelem=m_gin->read_whole_block((char*&)pVEL, 
+			"VEL ")/3;
+    m_gin->read_whole_block((char*&)pID, 
+			"ID  ");
+    nwithmass=m_gin->read_whole_block((char*&)pMASS, 
+			   "MASS");
+
     
     if(nelem==0)
       {  cout<<"Error reading block:"<<"POS"<<
@@ -175,20 +187,21 @@ public:
 			     "BHMA","BHMD", "MACH", 
 			     "DISSE"
     };
-    int nb =32;
+    int nb =31;
     for(int i=0;i<nb;i++)
       if(m_gin->find_block(&m_gin->m_file,allblocks[i])>1 )
 	{
-	  cout<<allblocks[i]<<endl;
+	  cout<<"#PRESENT =>  "<<allblocks[i]<<endl;
 	  which_blocks.push_back(allblocks[i]);
 	}
     
   }
+
+ CGadget *m_gin;
 protected:
   int m_type;
   unsigned int nelem;
   string m_infile;
-  CGadget *m_gin;
   int m_isnap;
   std::map<int, int> ID;
   int ntotal;
@@ -196,7 +209,9 @@ public:
   int *pID;
   UCHAR *pTYPE;
   float *pPOS;
-
+  float *pVEL;
+  float *pMASS;
+  int nwithmass;
 };
 
 //////////////////////////////
@@ -215,6 +230,7 @@ m_inifile(ini), m_path(path)
 	cout<<"#BoxSize/2="<<m_Box2<<endl;
 	m_com.resize(m_nhalo);
 	id_loc.resize(m_nhalo);
+
 	for(int hcur=0;hcur<m_nhalo;hcur++)
 	  {
 	    cout<<"# Anal:"<<hcur<<endl;
@@ -227,40 +243,205 @@ m_inifile(ini), m_path(path)
 	cout<<"#get Whole data"<<endl;
 	data->GetAllPos();
 	cout<<"# Fill ID location info with Np="<<data->GetNp()<<endl;
+	floc.resize(m_nhalo);
 	for(int hcur=0;hcur<m_nhalo;hcur++)
 	  {
+	    int im=0;// mass block counter
+	    int ig=0;// ig block counter	
+	    int iz=0;// iZ block counter
+	    int iage=0;// iage block counter
+
 	    for(int i=0;i<data->GetNp();i++)
-	      {
-		if( isInReg(&data->pPOS[i*3],hcur,data->pTYPE[i] ) )
+	      {	
+		myType=data->pTYPE[i];
+		//first setup counters
+		if(data->m_gin->myhead.mass[myType]==0 && 
+		       data->m_gin->myhead.npart[myType]>0)
 		  {
-		    myType=data->pTYPE[i];
+		    im++;
+		  }
+		if(myType==0)ig++;
+		if(myType==0||myType==4)iz++; 
+		if(myType==4)iage++;
+		//check region
+		if( isInReg(data->pPOS[i*3], data->pPOS[i*3+1], data->pPOS[i*3+2],hcur) )
+		  {		    
 		    id_loc[hcur].type_idx[myType].push_back(i);
+		    floc[hcur].mergedIDX.push_back(i);
 		    
+		   if(data->m_gin->myhead.mass[myType]==0 && 
+		       data->m_gin->myhead.npart[myType]>0)
+		     {
+		        floc[hcur].IDXWithMass.push_back(im);
+		     }
+		   if(myType==0)
+		      floc[hcur].IDXgas.push_back(ig);
+		   if(myType==0 || myType==4)
+		      floc[hcur].IDXz.push_back(iz); 
+		   if(myType==4)
+		     floc[hcur].IDXage.push_back(iage);  
 		  }
 
-	      }	   
+	      }
+	    
+	    
 	  }
-	      	    
+	
 	ReportHaloStats();
 	//////////////////////////////////////////////////////
 	//which blocks are present??
 	data->blocks(which_blocks);	
-	
+
 	//////////////////////////////////////////////////////
 	cout<<"# start to write dumps "<< m_dumpfile<<
 	  " found "<<which_blocks.size()<<" blocks."
 	    <<endl;
-		std::string snap=m_file.substr(m_file.find_last_of("_")+1,  
-			 m_file.find_last_of("_")+3);
-	char dumpname[256];
+
+	std::string snap=m_file.substr(m_file.find_last_of("_")+1,  
+				       m_file.find_last_of("_")+3);
+	char dumpname[1024];
 	for(int hcur=0;hcur<m_nhalo;hcur++)
 	  {	
 	    sprintf(&dumpname[0], m_dumpfile.c_str(),hcur,int(m_Rad), snap.c_str());
 	    cout<<"# "<<dumpname<<endl;
+	    
+	    cout<<"# got total in region:"<<floc[hcur].mergedIDX.size()<<endl;
+	    cout<<"# got Nwithmass:"<<floc[hcur].IDXWithMass.size()<<endl;
+	    cout<<"# got NwithZ:"<<floc[hcur].IDXz.size()<<endl;
+	
+	    ofstream file;	    
+	    file.open(dumpname,ios::binary);
+	    CGadget::io_header head;
+	    int totpart=0;
+	   
+	    CGadget *tg=new CGadget();
+	    memcpy(&head,&data->m_gin->myhead,sizeof(head));
+	    for(int it=0;it<6;it++)
+	      {
+		head.npart[it]=id_loc[hcur].type_idx[it].size();
+		head.npartTotal[it]=head.npart[it];
+		totpart+=head.npart[it];
+		if(head.npart[it]==0)
+		  head.mass[it]=0.0;
+	      }
+	    head.num_files=1;
+	    /////////////////////////////////////////////////////
+	    int blsize;
+	    blsize=sizeof(head);
+	    tg->WriteOneBlock(file,"HEAD", (char*)&head, blsize);
+	    
+	    //////////////////////////////////////////////////////
+	  
+	      {
+		{
+		    int idx=0, k=0;
+		    float *pPos=new float[totpart*3];
+		    float *pVel=new float[totpart*3];
+		    int *pID=new int[totpart];
+		   
+		
+		    for(int ip=0;ip<totpart;ip++)
+		      {
+			idx=floc[hcur].mergedIDX[ip];
+			pPos[k]=data->pPOS[idx*3];
+			pPos[k+1]=data->pPOS[idx*3+1];
+			pPos[k+2]=data->pPOS[idx*3+2];
+
+			pVel[k]=data->pVEL[idx*3];
+			pVel[k+1]=data->pVEL[idx*3+1];
+			pVel[k+2]=data->pVEL[idx*3+2];
+
+			pID[ip]=data->pID[idx];
+			
+			k+=3;
+		      }
+		    ///////////////////////////////
+		    cout<<"try to free some data"<<endl;
+		    cout<<"done"<<endl;
+		    //////////////////////
+		    blsize=sizeof(float)*totpart*3;		   
+		    tg->WriteOneBlock(file,string("POS "), (char*)pPos, blsize);	          
+		    tg->WriteOneBlock(file,string("VEL "), (char*)pVel, blsize);	 
+		    blsize=sizeof(int)*totpart;
+		    tg->WriteOneBlock(file,string("ID  "), (char*)pID, blsize);
+		    safeFree(pPos);		
+		    safeFree(pVel);
+		    safeFree(pID);
+		    for(int ib=1;ib<which_blocks.size();ib++)
+		    {
+		    ///////////////////////////////
+		    if(which_blocks[ib]=="MASS" && floc[hcur].IDXWithMass.size()>0)
+		      {
+			float *pMASS=
+			  pMASS=new float[floc[hcur].IDXWithMass.size()];
+			for(int ip=0;ip<floc[hcur].IDXWithMass.size();ip++)
+			  {
+			    pMASS[ip]=data->pMASS[floc[hcur].IDXWithMass[ip]];
+			  }
+			  blsize=sizeof(int)*floc[hcur].IDXWithMass.size();
+			  tg->WriteOneBlock(file,string("MASS"), (char*)pMASS, blsize);
+			  safeFree( pMASS);		    
+		      }
+		    if(which_blocks[ib]=="U   "||
+		       which_blocks[ib]=="RHO "||
+		       which_blocks[ib]=="NE  "||
+		       which_blocks[ib]=="NH  "||
+		       which_blocks[ib]=="HSML"||
+		       which_blocks[ib]=="SFR "
+		       )
+		      {
+			float *pBUF=new float[data->m_gin->myhead.npart[0]];
+			data->m_gin->read_whole_block((char*&)pBUF, 
+			   which_blocks[ib].c_str());			
+			for(int ip=0;ip<floc[hcur].IDXgas.size();ip++)
+			  {
+			    pBUF[ip]=pBUF[floc[hcur].IDXgas[ip]];
+			  }
+			blsize=sizeof(float)*floc[hcur].IDXgas.size();
+			tg->WriteOneBlock(file,which_blocks[ib], (char*)pBUF, blsize);
+			safeFree(pBUF);
+		      }
+		    if(which_blocks[ib]=="AGE ")
+		      {
+			float *pBUF=new float[data->m_gin->myhead.npart[4]];
+			data->m_gin->read_whole_block((char*&)pBUF, 
+			   which_blocks[ib].c_str());			
+			for(int ip=0;ip<floc[hcur].IDXage.size();ip++)
+			  {
+			    pBUF[ip]=pBUF[floc[hcur].IDXage[ip]];
+			  }
+			blsize=sizeof(float)*floc[hcur].IDXage.size();
+			tg->WriteOneBlock(file,which_blocks[ib], (char*)pBUF, blsize);
+			safeFree(pBUF);
+		      }
+		    if(which_blocks[ib]=="Z   ")
+		      {
+			float *pBUF=new float[data->m_gin->myhead.npart[0]+data->m_gin->myhead.npart[4]];
+			data->m_gin->read_whole_block((char*&)pBUF, 
+			   which_blocks[ib].c_str());			
+			for(int ip=0;ip<floc[hcur].IDXz.size();ip++)
+			  {
+			    pBUF[ip]=pBUF[floc[hcur].IDXz[ip]];
+			  }
+			blsize=sizeof(float)*floc[hcur].IDXz.size();
+			tg->WriteOneBlock(file,which_blocks[ib], (char*)pBUF, blsize);
+			safeFree(pBUF);
+		      }
+		    }
+		  
+		    file.close();		    
+		   		    
+		  }
+	      }	    
+	    //////////////////////////////////////////////////////
+	    
+
+	    safeFreeOne( tg);
+	    
 	  }
 	
 	/////////////////////////////////////
-	delete data;
+	safeFreeOne( data);
 	/////////////////////////////////////
 	cout<<"# DONE CExtractor"<<endl;
 	}
